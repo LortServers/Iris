763cca6644fe1abb6c807ef35649ce2b87ee9b3c
zlataovce <34477304+zlataovce@users.noreply.github.com>
2021-10-12 14:42:41 +0200

    work: removing apache commons and some fixups

--- plugin/src/main/java/net/lortservers/iris/config/TranslationManagerImpl.java
+++ plugin/src/main/java/net/lortservers/iris/config/TranslationManagerImpl.java
@@ -3,7 +3,6 @@ package net.lortservers.iris.config;
 import net.lortservers.iris.IrisPlugin;
 import net.lortservers.iris.api.managers.ConfigurationManager;
 import net.lortservers.iris.api.managers.TranslationManager;
-import org.apache.commons.lang3.LocaleUtils;
 import org.screamingsandals.lib.utils.annotations.Service;
 import org.screamingsandals.lib.utils.annotations.methods.OnEnable;
 
@@ -28,9 +27,12 @@ public class TranslationManagerImpl implements TranslationManager {
                 }
             }
             try {
-                currentTranslation = ConfigurationManagerImpl.MAPPER.readValue(IrisPlugin.class.getResourceAsStream("messages/" + LocaleUtils.toLocale(ConfigurationManager.getInstance().getValue("locale", String.class).orElse("en_US"))), Messages.class);
-            } catch (IOException | IllegalArgumentException e) {
-                IrisPlugin.getInstance().getLogger().info("Could not load translation (possibly invalid locale or serialization error)!");
+                final String locale = ConfigurationManager.getInstance().getValue("locale", String.class).orElse("en_US").replace("-", "_");
+                currentTranslation = ConfigurationManagerImpl.MAPPER.readValue(IrisPlugin.class.getResourceAsStream("messages/" + locale + ".json"), Messages.class);
+            } catch (IOException e) {
+                IrisPlugin.getInstance().getLogger().error("Could not load translation, deserialization error!", e);
+            } catch (IllegalArgumentException e) {
+                IrisPlugin.getInstance().getLogger().error("Could not load translation, invalid locale!");
             }
         });
     }
